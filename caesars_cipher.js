61e6d457adcb9b1e7e23e4b67ae18c0585890d56
Olivia Clark <oliviaclark27@gmail.com>
2022-06-09 13:38:04 -0400

    Edited for readability and comments

--- caesars_cipher.js
+++ caesars_cipher.js
@@ -1,7 +1,39 @@
 function rot13(str) {
-  let cipher = {'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q', 'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U', 'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y', 'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C', 'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G', 'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K', 'Y': 'L', 'Z': 'M', ' ': ' ', '!': '!', '?': '?', '.': '.'};
+  // build cipher object
+  let cipher = {
+    'A': 'N', 
+    'B': 'O', 
+    'C': 'P', 
+    'D': 'Q', 
+    'E': 'R', 
+    'F': 'S', 
+    'G': 'T', 
+    'H': 'U', 
+    'I': 'V', 
+    'J': 'W', 
+    'K': 'X', 
+    'L': 'Y', 
+    'M': 'Z', 
+    'N': 'A', 
+    'O': 'B', 
+    'P': 'C', 
+    'Q': 'D', 
+    'R': 'E', 
+    'S': 'F', 
+    'T': 'G', 
+    'U': 'H', 
+    'V': 'I', 
+    'W': 'J', 
+    'X': 'K', 
+    'Y': 'L', 
+    'Z': 'M', 
+    ' ': ' ', 
+    '!': '!', 
+    '?': '?', 
+    '.': '.'
+  };
   let newStr = [];
-
+  //access object with object entries
   for (let i = 0; i < str.length; i++) {
     let filtered = Object.fromEntries(Object.entries(cipher).filter(([key, value]) => key === str[i]));
     newStr.push(Object.values(filtered));
